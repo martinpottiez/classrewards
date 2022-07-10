5ef9ce7588a5e09d358a0d01b28d7642ea287a43
Martin Pottiez <martinpottiez@hotmail.fr>
2022-07-10 13:37:44 +0200

    [Register] Add form registration

--- model/loginModel.php
+++ model/loginModel.php
@@ -2,9 +2,9 @@
 
 require_once('model/configModel.php');
 
-class loginModel extends configModel{
+class loginModel extends configModel {
 
-  public function getUser($mail, $pass){
+  public function getUser($mail) {
     $db = $this->connect();
     $req = $db->prepare("SELECT * FROM users WHERE mail = :mail");
     $req->execute(array(
@@ -13,11 +13,11 @@ class loginModel extends configModel{
     return $req;
   }
 
-  public function is_login($mail, $pass){
-    $is_user = $this->getUser($mail, $pass);
-    if($is_user->rowCount() == 1){
+  public function is_login($mail, $pass) {
+    $is_user = $this->getUser($mail);
+    if($is_user->rowCount() == 1) {
       $user = $is_user->fetch();
-      if(password_verify($pass, $user['password'])){
+      if(password_verify($pass, $user['password'])) {
         $_SESSION['id'] = $user['id'];
         return true;
       } else {
@@ -25,6 +25,26 @@ class loginModel extends configModel{
       }
     } else {
       throw new Exception("Identifiants incorrects");
+    }
+  }
+
+  public function register($userInfos) {
+    $isUser = $this->getUser($userInfos[2]);
+    if($isUser->rowCount() == 0) {
+      $username = strtolower(substr($userInfos[1],0,2).$userInfos[0]);
+      $password = password_hash($userInfos[3], PASSWORD_DEFAULT);
+      $db = $this->connect();
+      $req = $db->prepare("INSERT INTO users (username, mail, password, name, surname) VALUES (:username, :mail, :password, :name, :surname)");
+      $req->execute(array(
+        'username' => $username,
+        'mail' => strtolower($userInfos[2]),
+        'password' => $password,
+        'name' => $userInfos[1],
+        'surname' => $userInfos[0]
+      ));
+      return true;
+    } else {
+      throw new Exception("Adresse mail déjà utilisée");
     }
   }
 }
--- view/template_offline.php
+++ view/template_offline.php
@@ -1,20 +1,23 @@
 <?php include('inc/head.php'); ?>
       <div class="container alignc-start bg-primary nav-col p20">
-        <div>
+        <div class="center">
           <a href="">
-            <img src="view/img/logo.png" alt="Logo de ClassRewards" class="logo"/>
+            <img src="view/img/logo.png" alt="Logo de JollyClass" class="logo"/>
           </a>
         </div>
         <div class="mt-20">
+          <div class="subtitleIcon colorLight"><i class="fas fa-sign-in-alt"></i><span class="">Connexion</span></div>
           <div><?php if(isset($error)){echo $error;}?></div>
           <form method="post" action="index/login">
-            <input type="text" placeholder="Email" name="mail"/>
+            <input type="text" placeholder="Adresse mail" name="mail"/>
             <input type="password" placeholder="Mot de passe" name="password"/>
-            <input type="submit" value="Connexion" name="login"/>
+            <input type="submit" value="Connexion" class="bg-lightPrimary" name="login"/>
           </form>
+          <div class="center"><a href="">Identifiants oubliés</a></div>
         </div>
       </div>
-      <div class="fg-1">
+      <div class="container fd-column fg-1 p20">
+        <?= $content ?>
       </div>
     </div>
   </body>
